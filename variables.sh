4d0c7643bd797f363e0fa7e5d54112bbcc842181
valli25 <pravallika.tsm@gmail.com>
2024-08-27 14:24:11 -0700

    adding files

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Nirman
+PERSON2=Sahan
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "PERSON2: Hi $PERSON1, Very good morning"
+echo "PERSON1: how are you? $PERSON2"
+echo "$PERSON2: I'm good $PERSON1, how are you;
