fe3a0d4d63c7e52bef445a9997bee1f22d7bff92
valli25 <pravallika.tsm@gmail.com>
2024-08-28 11:17:04 -0700

    adding files

--- install-mysql.sh
+++ install-mysql.sh
@@ -2,10 +2,18 @@
 ID=(id -u)
 if [ $ID -ne 0 ]
 then
-  echo "ERROR:: please run this script with root access"
+  echo "ERROR: "please run this script with root access"
   exit 1
 else  
   echo "you are root user"
 fi 
 
-yum install mysql -y
+yum  install mysqli -y
+
+if [ $? -ne 0 ]
+then 
+   echo "ERROR: "Installing mysql failed"
+   exit 1
+else 
+  echo "installing mysql is success"
+fi
