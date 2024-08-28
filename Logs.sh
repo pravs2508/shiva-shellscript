4fac7967e35566a6d9636e09e6dfcc3e7e77330d
valli25 <pravallika.tsm@gmail.com>
2024-08-28 12:26:31 -0700

    adding files

--- Logs.sh
+++ Logs.sh
@@ -19,10 +19,10 @@ then
 else  
   echo "you are root user"
 fi 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE 
 
 VALIDATE $? "Installing mysql"
 
-yum install git -y
+yum install git -y &>> $LOGFILE 
 
 VALIDATE $? "Installing git"
