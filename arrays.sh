2ad82cbfd5961dd401d22d7d9f2ced444e42a9c8
valli25 <pravallika.tsm@gmail.com>
2024-08-28 10:48:04 -0700

    adding files

---
+++ arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FRUITS=("apples" "bananas" "mangos")
+
+echo "First value: ${FRUITS[0]}"
+
+echo "Second value: ${FRUITS[1]}"
+ 
+echo "third value: ${FRUITS[2]}"
+
+echo "All Fruits: ${FRUITS[@]}"
