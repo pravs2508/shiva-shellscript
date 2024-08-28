1fbc9f950a74eac32d57b157e0b34ef8deca3ddc
valli25 <pravallika.tsm@gmail.com>
2024-08-28 12:38:07 -0700

    adding files

--- Logs.sh
+++ Logs.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 ID=(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $? -ne 0 ]
 then 
-   echo "ERROR:: $2 ... failed"
+   echo  - e "ERROR:: $2 ... $R failed $N"
    exit 1
 else 
-  echo "$2 ... success"
+  echo -e "$2 ... $G success $N"
 fi
 } 
 
 if [ $ID -ne 0 ]
 then
-  echo "ERROR:: please run this script with root access"
+  echo -e "$R ERROR:: please run this script with root access $N"
   exit 1
 else  
   echo "you are root user"
