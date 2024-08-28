de0f18463d6e874747bd643e86252c9353b55ada
valli25 <pravallika.tsm@gmail.com>
2024-08-28 12:24:30 -0700

    adding files

---
+++ Logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ID=(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%s)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+    if [ $? -ne 0 ]
+then 
+   echo "ERROR:: $2 ... failed"
+   exit 1
+else 
+  echo "$2 ... success"
+fi
+} 
+
+if [ $ID -ne 0 ]
+then
+  echo "ERROR:: please run this script with root access"
+  exit 1
+else  
+  echo "you are root user"
+fi 
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
